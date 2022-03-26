b7b2bfb3d734ed8029dcb5be43601be858071627
zbxytx <838374029@qq.com>
2022-03-26 14:08:45 +0800

    SiTu branch

--- utils/loss.py
+++ utils/loss.py
@@ -1,107 +1,66 @@
-import time
 import numpy as np
-from sklearn.metrics import recall_score, f1_score, accuracy_score, confusion_matrix, precision_score
-
 import torch
-from torch import nn
-import os
+import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 
 class CCCLoss(nn.Module):
-    def __init__(self, reduction='mean', batch_first=True, batch_compute=False):
-        super(CCCLoss, self).__init__()
-        self.reduction = reduction
-        self.batch_first = batch_first
-        self.batch_compute = batch_compute
+    def __init__(self, digitize_num, range=[-1, 1], eps=1e-8):
+        super(CCCLoss, self).__init__() 
+        self.digitize_num =  digitize_num
+        self.range = range
+        self.eps=eps
+        if self.digitize_num !=0:
+            bins = np.linspace(*self.range, num= self.digitize_num)
+            self.bins = Variable(torch.as_tensor(bins, dtype = torch.float32).cuda()).view((1, -1))
 
-    def forward(self, inputs, target, mask=None):
-        if mask is not None:
-            if self.batch_compute:
-                inputs, target = inputs[mask != 0], target[mask != 0]
-            else:
-                inputs, target = inputs * mask, target * mask
-
-        if self.batch_compute:
-            inputs, target = inputs.reshape(1, -1), target.reshape(1, -1)
-        a_mean, b_mean = torch.mean(inputs, dim=1), torch.mean(target, dim=1)
-        a_var = torch.mean(torch.square(inputs), dim=1)-torch.square(a_mean)
-        b_var = torch.mean(torch.square(target), dim=1)-torch.square(b_mean)
-        cor_ab = torch.mean((inputs - a_mean.unsqueeze(1))*(target - b_mean.unsqueeze(1)), dim=1)
-
-        ccc = 2 * cor_ab / (a_var + b_var + torch.square(a_mean - b_mean) + 1e-9)
-        #batch_size, length = target.shape[0], target.shape[1]
-        #if not self.batch_compute:
-        #    loss = torch.sum((1 - ccc) * torch.sum(mask, dim=1) / length)
-        #    if self.reduction == 'mean':
-        #        loss = loss / torch.sum(mask) * length
-        #else:
-        #    loss = torch.sum(1-ccc) if self.reduction == 'sum' else torch.mean(1-ccc)
-        loss = torch.sum(1 - ccc) if self.reduction == 'sum' else torch.mean(1 - ccc)
-        return loss
-
-
-class MSELoss(nn.Module):
-    def __init__(self, reduction='mean'):
-        super(MSELoss, self).__init__()
-        self.mse = torch.nn.MSELoss(reduction=reduction)
-
-    def forward(self, inputs, target, mask=None):
-        if mask is not None:
-            return self.mse(inputs * mask, target * mask)
-        else:
-            return self.mse(inputs, target)
-
+    def forward(self, x, y): 
+        y = y.view(-1)
+        if self.digitize_num !=1:
+            x = F.softmax(x, dim=-1)
+            x = (self.bins * x).sum(-1)
+        x = x.view(-1)
+        vx = x - torch.mean(x) 
+        vy = y - torch.mean(y) 
+        rho =  torch.sum(vx * vy) / (torch.sqrt(torch.sum(torch.pow(vx, 2))) * torch.sqrt(torch.sum(torch.pow(vy, 2)))+self.eps)
+        x_m = torch.mean(x)
+        y_m = torch.mean(y)
+        x_s = torch.std(x)
+        y_s = torch.std(y)
+        ccc = 2*rho*x_s*y_s/(torch.pow(x_s, 2) + torch.pow(y_s, 2) + torch.pow(x_m - y_m, 2))
+        return 1-ccc
 
 class CELoss(nn.Module):
-    def __init__(self, reduction='mean', ignore_index=-1):
-        super(CELoss, self).__init__()
-        self.ce = torch.nn.CrossEntropyLoss(reduction=reduction, ignore_index=ignore_index)
+    def __init__(self, digitize_num, range=[-1, 1], weights=None):
+        super(CELoss, self).__init__() 
+        self.digitize_num = digitize_num
+        if weights is not None:
+            self.weights = torch.Tensor(weights).cuda()
+        else:
+            self.weights = None
+        assert self.digitize_num !=1
+        self.edges = np.linspace(*range, num= self.digitize_num+1)
+    
+    def forward(self, x, y): 
+        y = y.view(-1)
+        y_numpy = y.data.cpu().numpy()
+        y_dig = np.digitize(y_numpy, self.edges) - 1
+        y_dig[y_dig==self.digitize_num] = self.digitize_num -1
+        y = Variable(torch.cuda.LongTensor(y_dig))
+        return F.cross_entropy(x, y, weight=self.weights)
 
-    def forward(self, inputs, target, mask=None):
-        #logits: [B, 22, L], target: [B, L], mask: [B, L]
-        if mask is not None:
-            target[mask == 0] = -1
-        return self.ce(inputs, target)
+class CCC_CE_Loss(nn.Module):
+    def __init__(self, digitize_num, range=[-1, 1], alpha=0.5, beta=0.5):
+        super(CCC_CE_Loss, self).__init__()
+        self.ccc_loss = CCCLoss(digitize_num, range=range)
+        self.ce_loss = CELoss(digitize_num, range=range)
+        self.alpha = alpha
+        self.beta = beta
 
-
-class MultipleLoss(nn.Module):
-    def __init__(self, reduction='mean', loss_names=('mse', ), weights=(1, )):
-        super(MultipleLoss, self).__init__()
-        self.reduction = reduction
-        self.loss_names = loss_names
-        self.weights = weights
-        self.loss_functions = []
-        for name in loss_names:
-            if 'mse' in name:
-                self.loss_functions.append(MSELoss(reduction='sum'))
-            elif 'ccc' in name:
-                batch_compute = True if 'batch' in name else False
-                self.loss_functions.append(CCCLoss(reduction='mean', batch_compute=batch_compute))
-            elif 'ce' in name:
-                self.loss_functions.append(CELoss(reduction='mean', ignore_index=-1))
-
-    def forward(self, inputs, target, mask=None, logits=None, cls_target=None):
-        loss = 0
-        ce_loss = 0
-        for i, (name, function) in enumerate(zip(self.loss_names, self.loss_functions)):
-            if name in ['vmse', 'vccc', 'batch_vccc']:
-                loss = self.weights[i] * function(inputs[..., 0], target[..., 0], mask[..., 0]) + loss
-            elif name in ['amse', 'accc', 'batch_accc']:
-                loss = self.weights[i] * function(inputs[..., -1], target[..., -1], mask[..., -1]) + loss
-            elif name == 'ce':
-                for j in range(inputs.shape[-1]):
-                    loss = self.weights[i] * \
-                           function(logits[..., j], cls_target[..., j], mask[..., j]) / inputs.shape[-1] + loss
-        #            ce_loss = self.weights[i] * \
-        #                   function(logits[..., j], cls_target[..., j], mask[..., j]) / inputs.shape[-1] + ce_loss
-            else:
-                for j in range(inputs.shape[-1]):
-                    loss = self.weights[i] * \
-                           function(inputs[..., j], target[..., j], mask[..., j]) / inputs.shape[-1] + loss
-
-        #print(loss, ce_loss)
-        
-        loss = (torch.tensor(0.0, requires_grad=True) if loss==0 else loss)##不加这句会报错
-        
+    def forward(self, x, y):
+        cccl = self.ccc_loss(x, y)
+        cel = self.ce_loss(x, y)
+        print(cccl, cel)
+        loss = self.alpha*cccl + self.beta*cel
         return loss
--- utils/path.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def make_path(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
